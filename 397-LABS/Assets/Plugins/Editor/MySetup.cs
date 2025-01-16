62445371dd3906dd659717091240e870c4c74668
YuHsuanChen <ddy20824@gmail.com>
2025-01-15 20:29:27 -0500

    Lab #2 - Changed platform to WebGL, Added InputSystemActions support and player controller.

---
+++ 397-LABS/Assets/Plugins/Editor/MySetup.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class MySetUp
+{
+    [MenuItem("Setup/Create Folders")]
+    public static void CreateMyFolders()
+    {
+        Folder.CreateFolders("_Project", "Animations", "Art", "Audio", "Fonts",
+        "Materials", "Prefabs", "ScriptableObjects", "Scripts", "Settings");
+
+        AssetDatabase.Refresh();
+    }
+    static class Folder
+    {
+        public static void CreateFolders(string root, params string[] folders)
+        {
+            var fullPath = Path.Combine(Application.dataPath, root);
+            foreach (string folder in folders)
+            {
+                var folderPath = Path.Combine(fullPath, folder);
+                if (!Directory.Exists(folderPath))
+                {
+                    Directory.CreateDirectory(folderPath);
+                }
+            }
+        }
+    }
+
+}
