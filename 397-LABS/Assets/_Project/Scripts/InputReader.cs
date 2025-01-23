0965425f45b3ad14a8928feab0d54324822177d6
YuHsuanChen <ddy20824@gmail.com>
2025-01-22 20:11:55 -0500

    Lab3 - Player Controller with Movement and Rotation. Cinemachine 3 added and Freelook camera setup.

--- 397-LABS/Assets/_Project/Scripts/InputReader.cs
+++ 397-LABS/Assets/_Project/Scripts/InputReader.cs
@@ -34,7 +34,6 @@ namespace Platformer397
                     Move?.Invoke(context.ReadValue<Vector2>());
                     break;
                 default:
-                    Debug.Log("No Input handled in phase:" + context.phase);
                     break;
             }
         }
